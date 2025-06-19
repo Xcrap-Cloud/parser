2a30696d86f35e810feed9c8e329a2b9cebaba53
Marcuth <thiago.playgames15@gmail.com>
2025-06-19 03:45:13 -0300

    adiciona suporte para valores aninhados no modelo de parsing JSON

--- src/json/parsing-model.ts
+++ src/json/parsing-model.ts
@@ -1,10 +1,14 @@
 import * as jmespath from "jmespath"
 
 import { ParsingModel } from "../parsing-model-interface"
+import { HtmlParsingModel } from "../html"
 
 export type JsonParsingModelShapeValue = {
     query: string
     default?: any
+    model?: ParsingModel
+    multiple?: boolean
+    limit?: number
 }
 
 export type JsonParsingModelShape = {
@@ -21,7 +25,13 @@ export class JsonParsingModel implements ParsingModel {
         for (const key in this.shape) {
             const value = this.shape[key]
 
-            data[key] = this.parseValue(value, root)
+            const isNestedValue = "model" in value
+
+            if (isNestedValue) {
+                data[key] = this.parseNestedValue(value, root)
+            } else {
+                data[key] = this.parseValue(value, root)
+            }
         }
 
         return data
@@ -32,6 +42,43 @@ export class JsonParsingModel implements ParsingModel {
 
         if (extractedData === null && value.default !== undefined) {
             return value.default
+        }
+
+        return extractedData
+    }
+
+    parseNestedValue(value: JsonParsingModelShapeValue, root: any) {
+        const extractedData = jmespath.search(root, value.query)
+        const model = value.model!
+        const modelIsHtmlParser = model.constructor.name === HtmlParsingModel.name
+
+        if (extractedData === null && value.default !== undefined) {
+            return value.default
+        }
+
+        if (value.multiple) {
+            if (!Array.isArray(extractedData)) {
+                throw new Error(`Expected an array for multiple values, but got ${typeof extractedData}`)
+            }
+
+            if (value.limit !== undefined) {
+                extractedData.splice(value.limit)
+            }
+
+            if (modelIsHtmlParser) {
+                if (extractedData.some(item => typeof item !== "string")) {
+                    throw new Error(`Expected an array of strings for model parsing, but got ${typeof extractedData[0]}`)
+                }
+
+                return extractedData.map(item => model.parse(item))
+            }
+
+        } else {
+            if (modelIsHtmlParser && typeof extractedData !== "string") {
+                throw new Error(`Expected a string for model parsing, but got ${typeof extractedData}`)
+            }
+
+            return model.parse(extractedData)
         }
 
         return extractedData
