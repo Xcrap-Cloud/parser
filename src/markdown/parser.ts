2dccb7db85264eba1189e5c328fa3b80a3ef048f
Marcuth <thiago.playgames15@gmail.com>
2025-06-22 21:54:48 -0300

    corrige a ordem de renderização do HTML no MarkdownParser e MarkdownParsingModel para garantir a decodificação correta das entradas

--- src/markdown/parser.ts
+++ src/markdown/parser.ts
@@ -8,8 +8,8 @@ export class MarkdownParser extends HtmlParser {
 
     constructor(source: string) {
         const decodedInputSource = he.decode(source)
-        const htmlSource = markdownIt.render(`<!DOCTYPE html><html lang="en"><head><title>Markdown Document</title></head><body>${decodedInputSource}</body></html>`)
-        super(htmlSource)
+        const htmlSource = markdownIt.render(decodedInputSource)
+        super(`<!DOCTYPE html><html lang="en"><head><title>Markdown Document</title></head><body>${htmlSource}</body></html>`)
         this.markdownSource = source
     }
 }
--- src/markdown/parsing-model.ts
+++ src/markdown/parsing-model.ts
@@ -6,7 +6,7 @@ import { markdownIt } from "./constants"
 export class MarkdownParsingModel extends HtmlParsingModel {
     parse(source: string): Promise<any> {
         const decodedInputSource = he.decode(source)
-        const htmlSource = markdownIt.render(`<!DOCTYPE html><html lang="en"><head><title>Markdown Document</title></head><body>${decodedInputSource}</body></html>`)
-        return super.parse(htmlSource)
+        const htmlSource = markdownIt.render(decodedInputSource)
+        return super.parse(`<!DOCTYPE html><html lang="en"><head><title>Markdown Document</title></head><body>${htmlSource}</body></html>`)
     }
 }
